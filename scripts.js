6b6d588621bb0da4e7edddfe89b9edb40effd390
JadeYoRHa <jade.macmaster@uleth.ca>
2023-10-14 01:29:41 -0800

    added API functionality

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
 const xhr = new XMLHttpRequest();
-
-const baseURL = "example.com";
-const query = "?api_key=APIKEYHERE&count=5";
+const baseURL = "https://www.boredapi.com/api/"
+const query = "activity?type=recreational"
 const url = baseURL + query;
+
+xhr.open("GET", url, true);
+xhr.send(null);
+
+xhr.onload = function() {
+    console.log("it worked");
+    let activityCOST = JSON.parse(xhr.responseText);
+    console.log(activityCOST);
+}
+
