436c33c0b6f7a2d1783dff4af6d410e3eb59beba
JadeYoRHa <jade.macmaster@uleth.ca>
2023-10-14 22:59:53 -0800

    css mostly finished

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
 const xhr = new XMLHttpRequest();
 const baseURL = "https://www.boredapi.com/api/"
-const query = "activity?type=recreational"
+var query = "activity?type=relaxation"
 const url = baseURL + query;
 
 xhr.open("GET", url, true);
 xhr.send(null);
 
-xhr.onload = function() {
+xhr.onload = function () {
     console.log("it worked");
     let activityCOST = JSON.parse(xhr.responseText);
     console.log(activityCOST);
 }
-
