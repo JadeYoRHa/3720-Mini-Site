013be7404619d5173d09e1d0e2802041e1ac1587
MacMaster <jade.macmaster@b516-r2m7.uleth.ca>
2023-10-17 13:49:27 -0600

    finished API functionality

--- scripts.js
+++ scripts.js
@@ -1,26 +1,48 @@
 const xhr = new XMLHttpRequest();
 const baseURL = "https://www.boredapi.com/api/"
 var query = "activity?participants=4"
-const url = baseURL + query;
+var index = 2
 var activityTYPE;
 
-xhr.open("GET", url, true);
-xhr.send(null);
+//Editing Site On Click
+
+
+
+function changeQueryRecharge() {
+    query = "activity?type=relaxation";
+    callAPI();
+    //I want to trigger the charging animation of the furthest empty bar
+    
+}
+
+function changeQueryDeplete() {
+    query = "activity?participants=4"
+    callAPI();
+    //I want to reverse the charging animation of the furthest full bar
+    const element= document.getElementsByClassName("inner")
+    element[index].style.animationFillMode = "backwards";
+    // element[index].style.animationDirection = "reverse";
+    element[index].style.visibility = "hidden";
+    index --
+}
+
+// API functionality
+
+function callAPI() {
+    let url = baseURL + query;
+    xhr.open("GET", url, true);
+    xhr.send(null);
+}
 
 xhr.onload = function () {
     activityTYPE = JSON.parse(xhr.responseText);
     console.log(activityTYPE.activity);
-}
-console.log(query);
-
-function changeQueryRecharge() {
-    query = "activity?type=relaxation";
+    change_text();
 }
 
 function change_text() {
-
-    document.getElementById("demo").innerHTML = activityTYPE.activity;
+    document.getElementById("activityText").innerHTML = activityTYPE.activity;
+    console.log(query);
 }
 
 console.log(query);
-
