0f24f7c001377b30b2eebe2e29b68f33cb3ff798
JadeYoRHa <jade.macmaster@uleth.ca>
2023-10-16 22:55:47 -0800

    finishing API functionality later

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const xhr = new XMLHttpRequest();
 const baseURL = "https://www.boredapi.com/api/"
-var query = "activity?type=relaxation"
+var query = "activity?participants=4"
 const url = baseURL + query;
 var activityTYPE;
 
@@ -11,8 +11,16 @@ xhr.onload = function () {
     activityTYPE = JSON.parse(xhr.responseText);
     console.log(activityTYPE.activity);
 }
+console.log(query);
+
+function changeQueryRecharge() {
+    query = "activity?type=relaxation";
+}
 
 function change_text() {
-    
+
     document.getElementById("demo").innerHTML = activityTYPE.activity;
 }
+
+console.log(query);
+
